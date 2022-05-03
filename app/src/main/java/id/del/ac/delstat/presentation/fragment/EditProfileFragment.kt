0fec392282f35ce843e214014caac1ad885295cb
matthewalfredoo <matthewwalfredoo@gmail.com>
2022-05-04 00:44:20 +0700

    Edit Profile (UI-Only)
    Function has not been implemented yet

---
+++ app/src/main/java/id/del/ac/delstat/presentation/fragment/EditProfileFragment.kt
@@ -0,0 +1,246 @@
+package id.del.ac.delstat.presentation.fragment
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.widget.doOnTextChanged
+import androidx.lifecycle.asLiveData
+import com.bumptech.glide.Glide
+import com.canhub.cropper.CropImageContract
+import com.canhub.cropper.CropImageView
+import com.canhub.cropper.options
+import com.del.d3ti20.util.RealPathUtil
+import com.google.android.material.snackbar.Snackbar
+import id.del.ac.delstat.BuildConfig
+import id.del.ac.delstat.R
+import id.del.ac.delstat.data.preferences.UserPreferences
+import id.del.ac.delstat.databinding.FragmentEditProfileBinding
+import id.del.ac.delstat.presentation.activity.HomeActivity
+import id.del.ac.delstat.presentation.user.viewmodel.UserViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+
+class EditProfileFragment : Fragment() {
+    private lateinit var binding: FragmentEditProfileBinding
+    private lateinit var userViewModel: UserViewModel
+    private lateinit var userPreferences: UserPreferences
+
+    private lateinit var nama: String
+    private lateinit var email: String
+    private lateinit var noHp: String
+    private lateinit var jenjang: String
+
+    private var intentData: Intent? = null
+    private var selectedImageUri: Uri? = null
+    private var selectedImagePath: String? = null
+
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+        val granted = permissions.entries.all {
+            it.value == true
+        }
+        if (granted) {
+            Log.d("MyTag", "Access to external storage granted")
+            selectPhoto()
+        }
+    }
+
+    private val selectPhotoResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if(result.resultCode == Activity.RESULT_OK) {
+            intentData = result.data
+            if(intentData != null) {
+                selectedImageUri = intentData!!.data
+                selectPhotoResult()
+            }
+        }
+        else {
+            Snackbar.make(binding.root, "Terjadi kesalahan saat memilih foto", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+    private val cropPhotoLauncher = registerForActivityResult(CropImageContract()) { result ->
+        if(result.isSuccessful){
+            selectedImageUri = result.uriContent
+            selectedImagePath = result.getUriFilePath(requireContext())
+            cropPhotoResult()
+        }
+        else{
+            Snackbar.make(binding.root, "Batal mengubah foto.", Snackbar.LENGTH_SHORT).show()
+            Log.e("MyTag", "Crop image failed", result.error)
+        }
+    }
+
+    companion object {
+        var PERMISSIONS = arrayOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentEditProfileBinding.bind(view)
+        userViewModel = (activity as HomeActivity).userViewModel
+        userPreferences = (activity as HomeActivity).userPreferences
+
+        prepareUI()
+    }
+
+    private fun selectPhoto() {
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            Log.d("MyTag", "No permission needed, and do something")
+        }
+        activity?.let {
+            if(hasPermissions(requireContext(), PERMISSIONS)) {
+                Log.d("MyTag", "Permission granted, and do something")
+                val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    .setType("image/*")
+                selectPhotoResultLauncher.launch(intent)
+            }
+            else {
+                requestPermissionLauncher.launch(PERMISSIONS)
+            }
+        }
+    }
+
+    private fun cropPhoto() {
+        cropPhotoLauncher.launch(
+            options(uri = selectedImageUri) {
+                setGuidelines(CropImageView.Guidelines.ON)
+                setAspectRatio(1, 1)
+                setOutputCompressQuality(80)
+                setOutputCompressFormat(Bitmap.CompressFormat.PNG)
+            }
+        )
+    }
+
+    private fun selectPhotoResult() {
+        // binding.imageViewProfile.setImageURI(selectedImageUri)
+        Log.d("MyTag", "SelectPhotoResult: ${RealPathUtil.getRealPath(requireContext(), selectedImageUri!!)}")
+        cropPhoto()
+    }
+
+    private fun cropPhotoResult() {
+        binding.imageViewProfile.setImageURI(selectedImageUri)
+        Log.d("MyTag", "CropPhotoResult: $selectedImagePath")
+    }
+
+    private fun hasPermissions(context: Context, permissions: Array<String>): Boolean = permissions.all {
+        ActivityCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun prepareUI() {
+        runBlocking(Dispatchers.IO) {
+            nama = userPreferences.getUserNama.first()!!
+            email = userPreferences.getUserEmail.first()!!
+            noHp = userPreferences.getUserNoHp.first()!!
+            jenjang = userPreferences.getUserJenjang.first()!!
+        }
+
+        binding.apply {
+            editTextNamaEditProfile.setText(nama)
+            editTextEmailEditProfile.setText(email)
+            editTextNoHpEditProfile.setText(noHp)
+            editTextJenjangEditProfile.setText(jenjang)
+        }
+
+        binding.buttonEditProfile.setOnClickListener {
+            editProfile()
+        }
+
+        binding.imageViewProfile.setOnClickListener {
+            selectPhoto()
+        }
+
+        userPreferences.getUserFotoProfil.asLiveData().observe(viewLifecycleOwner) {
+            val fotoProfilUrl = BuildConfig.BASE_URL + it
+            Log.d("MyTag", "FotoProfilUrl: $fotoProfilUrl")
+            Glide.with(requireActivity().applicationContext)
+                .load(fotoProfilUrl)
+                .into(binding.imageViewProfile)
+        }
+
+        inputValidation()
+    }
+
+    private fun inputValidation() {
+        binding.editTextNamaEditProfile.doOnTextChanged { text, start, before, count ->
+            if (text == null || text.isEmpty()) {
+                binding.textInputLayout1.error = "Nama harus diisi"
+            } else {
+                binding.textInputLayout1.error = null
+            }
+        }
+
+        binding.editTextEmailEditProfile.doOnTextChanged { text, start, before, count ->
+            if (text == null || text.isEmpty()) {
+                binding.textInputLayout2.error = "Email harus diisi"
+            } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(text).matches()) {
+                binding.textInputLayout2.error = "Email tidak valid"
+            } else {
+                binding.textInputLayout2.error = null
+            }
+        }
+
+        binding.editTextNoHpEditProfile.doOnTextChanged { text, start, before, count ->
+            if (text == null || text.isEmpty()) {
+                binding.textInputLayout3.error = "Nomor telepon harus diisi"
+            } else if (text.length < 10) {
+                binding.textInputLayout3.error = "Nomor telepon tidak valid"
+            } else {
+                binding.textInputLayout3.error = null
+            }
+        }
+
+        binding.editTextJenjangEditProfile.doOnTextChanged { text, start, before, count ->
+            if (text == null || text.isEmpty()) {
+                binding.textInputLayout4.error = "Jenjang harus diisi"
+            } else {
+                binding.textInputLayout4.error = null
+            }
+        }
+    }
+
+    private fun checkInput(): Boolean {
+        val validInput =
+            binding.textInputLayout1.error == null &&
+                    binding.textInputLayout2.error == null &&
+                    binding.textInputLayout3.error == null &&
+                    binding.textInputLayout4.error == null
+
+        return validInput
+    }
+
+    private fun editProfile() {
+        if (checkInput()) {
+            Log.d("MyTag", "Edit Profile")
+            Log.d("MyTag", binding.editTextNamaEditProfile.text.toString())
+            Log.d("MyTag", binding.editTextEmailEditProfile.text.toString())
+            Log.d("MyTag", binding.editTextNoHpEditProfile.text.toString())
+            Log.d("MyTag", binding.editTextJenjangEditProfile.text.toString())
+        }
+    }
+
+}
--- app/src/main/java/id/del/ac/delstat/presentation/fragment/ProfileFragment.kt
+++ app/src/main/java/id/del/ac/delstat/presentation/fragment/ProfileFragment.kt
@@ -8,24 +8,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.asLiveData
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
 import id.del.ac.delstat.BuildConfig
 import id.del.ac.delstat.R
 import id.del.ac.delstat.data.preferences.UserPreferences
 import id.del.ac.delstat.databinding.FragmentProfileBinding
 import id.del.ac.delstat.presentation.activity.HomeActivity
 import id.del.ac.delstat.presentation.activity.LoginActivity
-import id.del.ac.delstat.presentation.activity.MainActivity
 import id.del.ac.delstat.presentation.user.viewmodel.UserViewModel
 
 
 class ProfileFragment : Fragment() {
     private lateinit var userViewModel: UserViewModel
-    private lateinit var fragmentProfileBinding: FragmentProfileBinding
+    private lateinit var binding: FragmentProfileBinding
     private lateinit var userPreferences: UserPreferences
     private var bearerToken: String? = null
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,30 +36,23 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        fragmentProfileBinding = FragmentProfileBinding.bind(view)
+        binding = FragmentProfileBinding.bind(view)
         userViewModel = (activity as HomeActivity).userViewModel
         userPreferences = (activity as HomeActivity).userPreferences
 
         userPreferences.getUserToken.asLiveData().observe(viewLifecycleOwner) {
-            Log.d("MyTag", "ProfileFragment Token: $it")
+            // Log.d("MyTag", "ProfileFragment Token: $it")
             if (it.isNullOrEmpty()) {
-                fragmentProfileBinding.containerNotLogin.visibility = View.VISIBLE
-                fragmentProfileBinding.containerLogin.visibility = View.GONE
+                binding.containerNotLogin.visibility = View.VISIBLE
+                binding.containerLogin.visibility = View.GONE
             } else {
-                fragmentProfileBinding.containerLogin.visibility = View.VISIBLE
-                fragmentProfileBinding.containerNotLogin.visibility = View.GONE
+                binding.containerLogin.visibility = View.VISIBLE
+                binding.containerNotLogin.visibility = View.GONE
                 bearerToken = it
-                prepareUI()
             }
         }
 
-        fragmentProfileBinding.buttonLogout.setOnClickListener {
-            logout()
-        }
-
-        fragmentProfileBinding.buttonLogin.setOnClickListener {
-            startActivity(Intent(activity, LoginActivity::class.java))
-        }
+        prepareUI()
     }
 
     private fun logout() {
@@ -71,23 +62,36 @@ class ProfileFragment : Fragment() {
 
     private fun prepareUI() {
         userPreferences.getUserNama.asLiveData().observe(viewLifecycleOwner) {
-            fragmentProfileBinding.textViewNamaProfile.text = it
+            binding.textViewNamaProfile.text = it
         }
         userPreferences.getUserEmail.asLiveData().observe(viewLifecycleOwner) {
-            fragmentProfileBinding.textViewEmailProfile.text = it
+            binding.textViewEmailProfile.text = it
         }
         userPreferences.getUserNoHp.asLiveData().observe(viewLifecycleOwner) {
-            fragmentProfileBinding.textViewNoHpProfile.text = it
+            binding.textViewNoHpProfile.text = it
         }
         userPreferences.getUserJenjang.asLiveData().observe(viewLifecycleOwner) {
-            fragmentProfileBinding.textViewJenjangProfile.text = it
+            binding.textViewJenjangProfile.text = it
         }
         userPreferences.getUserFotoProfil.asLiveData().observe(viewLifecycleOwner) {
             val fotoProfilUrl = BuildConfig.BASE_URL + it
             Log.d("MyTag", "FotoProfilUrl: $fotoProfilUrl")
             Glide.with(requireActivity().applicationContext)
                 .load(fotoProfilUrl)
-                .into(fragmentProfileBinding.imageViewProfile)
+                .into(binding.imageViewProfile)
+        }
+
+        binding.buttonLogin.setOnClickListener {
+            // Log.d("MyTag", "Button Login Clicked")
+            startActivity(Intent(activity, LoginActivity::class.java))
+        }
+
+        binding.buttonLogout.setOnClickListener {
+            logout()
+        }
+
+        binding.buttonEditProfile.setOnClickListener {
+            findNavController().navigate(R.id.action_profileFragment_to_editProfileFragment)
         }
     }
 
