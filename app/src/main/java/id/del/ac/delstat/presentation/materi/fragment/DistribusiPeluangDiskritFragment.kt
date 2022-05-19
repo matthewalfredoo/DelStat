394929b369747b76277442447f4bfc1ae084b593
matthewalfredoo <matthewwalfredoo@gmail.com>
2022-05-19 20:25:55 +0700

    Fragment Materi Distribusi Peluang Diskrit

--- app/src/main/java/id/del/ac/delstat/presentation/materi/fragment/DistribusiPeluangDiskritFragment.kt
+++ app/src/main/java/id/del/ac/delstat/presentation/materi/fragment/DistribusiPeluangDiskritFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.size
+import com.davemorrissey.labs.subscaleview.ImageSource
 import id.del.ac.delstat.R
 import id.del.ac.delstat.databinding.FragmentDistribusiPeluangDiskritBinding
 
@@ -28,8 +30,22 @@ class DistribusiPeluangDiskritFragment : Fragment() {
     }
 
     private fun prepareUI() {
-
+        binding.imageViewDistribusiPeluangDiskrit1.setImage(ImageSource.resource(R.drawable.distribusi_peluang_diskrit_1))
+        binding.imageViewDistribusiPeluangDiskrit2.setImage(ImageSource.resource(R.drawable.distribusi_peluang_diskrit_2))
+        binding.imageViewDistribusiPeluangDiskrit3.setImage(ImageSource.resource(R.drawable.distribusi_peluang_diskrit_3))
+        binding.imageViewDistribusiPeluangDiskrit4.setImage(ImageSource.resource(R.drawable.distribusi_peluang_diskrit_4))
+        binding.imageViewDistribusiPeluangDiskrit5.setImage(ImageSource.resource(R.drawable.distribusi_peluang_diskrit_5))
+        binding.imageViewDistribusiPeluangDiskrit6.setImage(ImageSource.resource(R.drawable.distribusi_peluang_diskrit_6))
+        binding.imageViewDistribusiPeluangDiskrit7a.setImage(ImageSource.resource(R.drawable.distribusi_peluang_diskrit_7a))
+        binding.imageViewDistribusiPeluangDiskrit7b.setImage(ImageSource.resource(R.drawable.distribusi_peluang_diskrit_7b))
+        binding.imageViewDistribusiPeluangDiskrit8.setImage(ImageSource.resource(R.drawable.distribusi_peluang_diskrit_8))
     }
+
+    /*private fun renderMathViews() {
+        val formula1 = "x_{i\\ }=data\\ ke-i\\\\k = banyaknya \\space data"
+        binding.mathView1.formula = formula1
+        binding.mathView1.zoomOut()
+    }*/
 
     override fun onDestroy() {
         super.onDestroy()
