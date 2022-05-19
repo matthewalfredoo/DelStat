15d47a7c7e1795460162e86346c5eed234241229
matthewalfredoo <matthewwalfredoo@gmail.com>
2022-05-19 15:51:07 +0700

    All Empty Fragments of Materi

--- app/src/main/java/id/del/ac/delstat/presentation/materi/fragment/DistribusiPeluangDiskritFragment.kt
+++ app/src/main/java/id/del/ac/delstat/presentation/materi/fragment/DistribusiPeluangDiskritFragment.kt
@@ -1,38 +1,39 @@
-package id.del.ac.delstat.presentation.fragment
+package id.del.ac.delstat.presentation.materi.fragment
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import id.del.ac.delstat.R
-import id.del.ac.delstat.databinding.FragmentMateriBinding
-import id.del.ac.delstat.presentation.testactivity.MateriActivity
+import id.del.ac.delstat.databinding.FragmentDistribusiPeluangDiskritBinding
 
-class MateriFragment : Fragment() {
-
-    private lateinit var binding: FragmentMateriBinding
+class DistribusiPeluangDiskritFragment : Fragment() {
+    private lateinit var binding: FragmentDistribusiPeluangDiskritBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_materi, container, false)
+        return inflater.inflate(R.layout.fragment_distribusi_peluang_diskrit, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = FragmentMateriBinding.bind(view)
+
+        binding = FragmentDistribusiPeluangDiskritBinding.bind(view)
 
         prepareUI()
     }
 
     private fun prepareUI() {
-        binding.tvTest.setOnClickListener {
-            startActivity(Intent(activity, MateriActivity::class.java))
-        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.youtubePlayerView.release()
     }
 
 }
---
+++ app/src/main/java/id/del/ac/delstat/presentation/materi/fragment/MateriFragment.kt
@@ -0,0 +1,66 @@
+package id.del.ac.delstat.presentation.materi.fragment
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import id.del.ac.delstat.R
+import id.del.ac.delstat.databinding.FragmentMateriBinding
+import id.del.ac.delstat.presentation.testactivity.MateriActivity
+
+class MateriFragment : Fragment() {
+
+    private lateinit var binding: FragmentMateriBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_materi, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMateriBinding.bind(view)
+
+        prepareUI()
+    }
+
+    private fun prepareUI() {
+        binding.bodyCardviewMateri1.setOnClickListener {
+            findNavController().navigate(R.id.action_materiFragment_to_konsepPeluangFragment)
+        }
+        binding.bodyCardviewMateri2.setOnClickListener {
+            findNavController().navigate(R.id.action_materiFragment_to_variabelAcakFragment)
+        }
+        binding.bodyCardviewMateri3.setOnClickListener {
+            findNavController().navigate(R.id.action_materiFragment_to_distribusiPeluangDiskritFragment)
+        }
+        binding.bodyCardviewMateri4.setOnClickListener {
+            findNavController().navigate(R.id.action_materiFragment_to_distribusiKontinuFragment)
+        }
+        binding.bodyCardviewMateri5.setOnClickListener {
+            findNavController().navigate(R.id.action_materiFragment_to_pengantarStatistikAnalisisDataFragment)
+        }
+        binding.bodyCardviewMateri6.setOnClickListener {
+            findNavController().navigate(R.id.action_materiFragment_to_teknikSamplingFragment)
+        }
+        binding.bodyCardviewMateri7.setOnClickListener {
+            findNavController().navigate(R.id.action_materiFragment_to_anovaFragment)
+        }
+        binding.bodyCardviewMateri8.setOnClickListener {
+            findNavController().navigate(R.id.action_materiFragment_to_estimasiSatuDanDuaSampelFragment)
+        }
+        binding.bodyCardviewMateri9.setOnClickListener {
+            findNavController().navigate(R.id.action_materiFragment_to_pengujianHipotesisFragment)
+        }
+        binding.bodyCardviewMateri10.setOnClickListener {
+            findNavController().navigate(R.id.action_materiFragment_to_regresiKorelasiFragment)
+        }
+    }
+
+}
